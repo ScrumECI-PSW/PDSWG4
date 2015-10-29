bbedffa3f2fc1312992efc88e133dac6eaf9b567
Andres Vega <2099340@linux82.labinfo.is.escuelaing.edu.co>
2015-10-29 17:04:36 -0500

    creacion fachada historia 1

---
+++ src/main/java/Logica/Facade/Facade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica.Facade;
+
+import Logica.ReporteProblema;
+import Logica.SolicitudSoftware;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author 2099340
+ */
+public class Facade {
+    
+    
+    public void registrarReporte(ReporteProblema pr){
+        
+    }
+    
+    public LinkedList<ReporteProblema> consultarProblemas(){
+        return null;
+    }
+    
+    public LinkedList<SolicitudSoftware> consultarSolicitudesSoftware(){
+        return null;
+    }
+}
